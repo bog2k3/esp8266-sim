fa8eab94800d1df06549d48577994fd3c7356d27
Bogdan Ionita <bog2k3@gmail.com>
2019-10-09 13:13:20 +0300

    fixed windows build

--- config.cmake
+++ config.cmake
@@ -1,11 +1,17 @@
 # this is the path to your PLATFORMIO folder where all sdks and libs are
-set ( 
-    PLATFORMIO_PATH 
-    /home/bog/.platformio
+set (
+    PLATFORMIO_PATH
+    C:/Users/bionita/.platformio
     )
 
 # this is the path to your own project that you want to simulate
-set( 
-    USER_SRC_PATH 
-    /home/bog/emu/usr/settingsmanager
+set(
+    USER_SRC_PATH
+    C:/Users/bionita/work/sergiu/settingsmanager
     )
+
+# for WINDOWS only: where you have MinGW installed
+set(
+	MINGW_PATH
+	C:/mingw64
+)
